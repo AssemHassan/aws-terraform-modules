e49ba6fed778e2b37eeeb33226433a36afccc012
assemhassan@gmail.com <assemhassan@gmail.com>
2023-10-19 11:10:47 +0300

    batch job options

--- main.tf
+++ main.tf
@@ -26,76 +26,76 @@ module "discovery" {
 #   lambda_role_arn = module.discovery.lambda_role.arn
 # }
 
-module "fg-cluster" {
-   source = "./modules/fg-cluster"
-   namespace = var.app.app_name 
-   container_host_port = 80 #world
-   container_port = 80 #internal
-   ecs_task_execution_role_arn = module.discovery.container_role.arn
-   ecs_task_role_arn = module.discovery.container_role.arn
-   container_image = "public.ecr.aws/ubuntu/nginx:latest"
-   container_name = "nginx" #"${var.app.app_name}-service"
+# module "fg-cluster" {
+#    source = "./modules/fg-cluster"
+#    namespace = var.app.app_name 
+#    container_host_port = 80 #world
+#    container_port = 80 #internal
+#    ecs_task_execution_role_arn = module.discovery.container_role.arn
+#    ecs_task_role_arn = module.discovery.container_role.arn
+#    container_image = "public.ecr.aws/ubuntu/nginx:latest"
+#    container_name = "nginx" #"${var.app.app_name}-service"
 
-}
-
-locals {
-  domain_name = "${var.app.app_name}.myit"
-}
-
-module "self-signed-cert-non-prod" {
-  source = "./modules/self-signed-cert-non-prod"
-  domain_name =local.domain_name
-  
-}
-
-
-module "cognito-user-pool" {
-  source = "./modules/cognito-userpool"
-  namespace = var.app.app_name
-  env       = var.app.env
-}
-
-module "cognito-appclient" {
-  source          = "./modules/cognito-app"
-  user_pool_id    = module.cognito-user-pool.user_pool.id
-  namespace = var.app.app_name
-  app_client_list = [
-    {
-      env_name      = "dev"
-      signout_urls  = ["http://localhost:8080/signout.html", "https://localhost:8081/signout.html", "https://${local.domain_name}/signout.html"]
-      callback_urls = ["http://localhost:8080/index.html", "https://localhost:8081/index.html", "https://${local.domain_name}/index.html"]
-    }]
-}
-
-
-module "elb-fargate-service" {
-  source = "./modules/elb-fargate-service"
-  namespace = var.app.app_name
-  ecs_cluster = module.fg-cluster.main_cluster
-  service-taskDefinition = module.fg-cluster.service-taskDefinition
-  container_host_port = 80
-  container_port = 80
-  container_subnets = module.discovery.container_subnets.ids
-  security_groups = [module.discovery.default_sg]
-  vpc_id = module.discovery.vpc_id 
-  health_check_path = "/"
-  cognito_user_pool_arn = module.cognito-user-pool.user_pool.arn
-  cognito_user_pool_domain = module.cognito-user-pool.user_pool_domain.domain
-  cognito_user_pool_client_id = module.cognito-appclient.userpool_clients.dev.id
-
-  cert_arn = module.self-signed-cert-non-prod.cert_arn
-  container_name = "nginx" #"${var.app.app_name}-service"
-}
-
-# module "batch-jobs" {
-#   source = "./modules/batch-jobs"
-#   namespace = var.app.app_name
-#   ecs_task_execution_role_arn = module.discovery.ec2_role.arn
-#   ecs_task_role_arn = module.discovery.ec2_instance_profile.arn
-#   batch_service_role_arn = module.discovery.batch_service_role.arn
-   
-#   container_image = "nginx:latest"  
-#   security_groups = [module.discovery.default_sg]
-#   container_subnets = module.discovery.container_subnets.ids
 # }
+
+# locals {
+#   domain_name = "${var.app.app_name}.myit"
+# }
+
+# module "self-signed-cert-non-prod" {
+#   source = "./modules/self-signed-cert-non-prod"
+#   domain_name =local.domain_name
+  
+# }
+
+
+# module "cognito-user-pool" {
+#   source = "./modules/cognito-userpool"
+#   namespace = var.app.app_name
+#   env       = var.app.env
+# }
+
+# module "cognito-appclient" {
+#   source          = "./modules/cognito-app"
+#   user_pool_id    = module.cognito-user-pool.user_pool.id
+#   namespace = var.app.app_name
+#   app_client_list = [
+#     {
+#       env_name      = "dev"
+#       signout_urls  = ["http://localhost:8080/signout.html", "https://localhost:8081/signout.html", "https://${local.domain_name}/signout.html"]
+#       callback_urls = ["http://localhost:8080/index.html", "https://localhost:8081/index.html", "https://${local.domain_name}/index.html"]
+#     }]
+# }
+
+
+# module "elb-fargate-service" {
+#   source = "./modules/elb-fargate-service"
+#   namespace = var.app.app_name
+#   ecs_cluster = module.fg-cluster.main_cluster
+#   service-taskDefinition = module.fg-cluster.service-taskDefinition
+#   container_host_port = 80
+#   container_port = 80
+#   container_subnets = module.discovery.container_subnets.ids
+#   security_groups = [module.discovery.default_sg]
+#   vpc_id = module.discovery.vpc_id 
+#   health_check_path = "/"
+#   cognito_user_pool_arn = module.cognito-user-pool.user_pool.arn
+#   cognito_user_pool_domain = module.cognito-user-pool.user_pool_domain.domain
+#   cognito_user_pool_client_id = module.cognito-appclient.userpool_clients.dev.id
+
+#   cert_arn = module.self-signed-cert-non-prod.cert_arn
+#   container_name = "nginx" #"${var.app.app_name}-service"
+# }
+
+module "batch-jobs" {
+  source = "./modules/batch-jobs"
+  namespace = var.app.app_name
+  ecs_task_execution_role_arn = module.discovery.ec2_role.arn
+  ecs_task_role_arn = module.discovery.ec2_instance_profile.arn
+  batch_service_role_arn = module.discovery.batch_service_role.arn
+   
+  container_image = "hello-world:latest"  
+  security_groups = [module.discovery.default_sg]
+  container_subnets = module.discovery.container_subnets.ids
+}
 
--- modules/batch-jobs/main.tf
+++ modules/batch-jobs/main.tf
@@ -20,23 +20,38 @@ resource "aws_batch_compute_environment" "comp_env" {
   
   compute_resources {
     ## 4 following Attributes are supported in case of type = "EC2"
-    #instance_role      = var.ecs_task_role_arn
-    #instance_type      = ["a1.medium"]
-    #min_vcpus          = 0
-    #desired_vcpus      = 1
+    instance_role      = var.ecs_task_role_arn
+    instance_type      = ["optimal"]
+    min_vcpus          = 0
+    desired_vcpus      = 0
 
     max_vcpus          = 2
     subnets = var.container_subnets
     security_group_ids = var.security_groups.*.id
-    type          = "FARGATE" #"EC2"
+    type          = "EC2" #"FARGATE" 
+  }
+}
+
+resource "aws_batch_scheduling_policy" "example" {
+  name = "example"
+
+  fair_share_policy {
+    compute_reservation = 1
+    share_decay_seconds = 60
+
+    share_distribution {
+      share_identifier = "default*"
+      weight_factor    = 0.5
+    }
   }
 }
 
 #Create a job queue for our batch job
 resource "aws_batch_job_queue" "job_queue" {
   name                      = "${var.namespace}-batch-job-queue"
+  scheduling_policy_arn = aws_batch_scheduling_policy.example.arn
   state                     = "ENABLED"
-  priority                  = 1  
+  priority                  = 99  
   compute_environments      = [aws_batch_compute_environment.comp_env.arn]
 }
  
